2fd953db8d4a9dde123a387cce88beadc9793597
einbocha <65666391+einbocha@users.noreply.github.com>
2026-03-19 05:18:46 +0100

    test

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import { OrbitControls } from '@react-three/drei';
 
 export default function Home() {
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <Canvas>
+    <div style={{ position: 'relative', width: '100vw', height: '100vh' }}>
+      <Canvas camera={{ position: [3, 2, 3], fov: 60 }}>
         <ambientLight intensity={0.5} />
         <directionalLight position={[2, 2, 5]} />
         <mesh>
@@ -14,6 +14,32 @@ export default function Home() {
         </mesh>
         <OrbitControls />
       </Canvas>
+      <div
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          pointerEvents: 'none',
+        }}
+      >
+        <h1
+          style={{
+            color: 'white',
+            fontSize: '6rem',
+            fontWeight: 'bold',
+            letterSpacing: '0.2em',
+            textTransform: 'uppercase',
+            margin: 0,
+          }}
+        >
+          logos
+        </h1>
+      </div>
     </div>
   );
 }
